81741896fac367640125974bbb2ead259bfcdfa2
RayanSf <rayansf18@gmail.com>
2024-02-06 17:01:41 -0300

    product-orderItem one-to-many association

--- src/main/java/com/dev/rayan/webservice/entities/Product.java
+++ src/main/java/com/dev/rayan/webservice/entities/Product.java
@@ -1,5 +1,6 @@
 package com.dev.rayan.webservice.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -37,12 +38,25 @@ public class Product implements Serializable {
     )
     private Set<Category> categories = new HashSet<>();
 
+    @Getter(value = AccessLevel.NONE)
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
+
     public Product(Long id, String name, String description, Double price, String imgUrl) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.price = price;
         this.imgUrl = imgUrl;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        for (OrderItem item : items) {
+            set.add(item.getOrder());
+        }
+        return set;
     }
 
 }
