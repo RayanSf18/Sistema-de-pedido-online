f3a4c65b2b281089851b15dfe2aceadfe1a76b56
RayanSf <rayansf18@gmail.com>
2024-02-06 09:09:23 -0300

    h2 database, test profile, jpa

--- src/main/java/com/dev/rayan/webservice/entities/User.java
+++ src/main/java/com/dev/rayan/webservice/entities/User.java
@@ -1,5 +1,6 @@
 package com.dev.rayan.webservice.entities;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.io.Serializable;
@@ -10,9 +11,13 @@ import java.io.Serializable;
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @ToString
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
     @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
